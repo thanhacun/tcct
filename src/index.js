cde3b791634698bef63159781c5d3e4e7de4f11c
THCN <thanhacun@gmail.com>
2018-01-11 13:31:56 +0700

    [UPD] update some modules, cleaning code, add separated file to load fontawesome

--- src/index.js
+++ src/index.js
@@ -7,23 +7,15 @@ import { ConnectedRouter } from 'react-router-redux';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
-
 import registerServiceWorker from './registerServiceWorker';
 import Menu from './components/Menu';
-// import PrintTho from './components/tcct/PrintTho';
 
 import store, { history } from './store';
 import tcctRoutes from './data/tcct';
 
-// === fontawesome 5.x.x ===
-import fontawesome from '@fortawesome/fontawesome';
-import { faChevronUp, faChevronDown, faTimesCircle, faPrint, faRandom,
-  faEdit, faSignInAlt, faUserPlus, faTimes, faTrashAlt, faSyncAlt, faSave } from '@fortawesome/fontawesome-free-solid';
-  import { faAlgolia, faFacebook, faGooglePlus, faGoogle } from '@fortawesome/fontawesome-free-brands';
-fontawesome.library.add(faChevronUp, faChevronDown, faTimesCircle, faPrint, faRandom,
-  faEdit, faAlgolia, faSignInAlt, faUserPlus, faTimes, faTrashAlt, faSyncAlt, faSave,
-  faFacebook, faGooglePlus, faGoogle);
-// === fontawesome 5.x.x ===
+/* === LOAD fontawesome === */
+import './components/fontawesomeLoading.js';
+/* === LOAD fontawesome === */
 
 const app = document.getElementById('root');
 // [] TODO: numbers of tho from a prop
@@ -35,13 +27,13 @@ ReactDOM.render(
       <Route exact path="/" key={`tcct_root`} render={() => <Redirect to="/tcct/xemtho/random" />} />
       <Route path={`/tcct/xemtho/random`} key={`tcct_random`} render={
         () => <Redirect to={`/tcct/xemtho/${store.getState().tcct.thoIndex.selectedIndex}`}/>}/>
-      {/* <Route exact path="/tcct/print/:index" component={PrintTho} key={`tcct_print`} /> */}
       {tcctRoutes}
     </div>
   </ConnectedRouter>
 </Provider>, app);
 
 // Only deploy registerServiceWorker in production mode
+// [] TODO: and only when having https
 if (process.env.NODE_ENV === 'production' && registerServiceWorker) {
   registerServiceWorker();
 }
