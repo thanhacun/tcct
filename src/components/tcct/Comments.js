2f324a2cde6dc0fd837791bec61f5f0b73eeb33d
THCN <thanhacun@gmail.com>
2018-02-03 10:34:49 +0700

    [ADD] enable markdown in comment
    [UPD] cleaning bundle size

--- src/components/tcct/Comments.js
+++ src/components/tcct/Comments.js
@@ -1,34 +1,37 @@
 import React from 'react';
 import { compose, withHandlers, withState, branch, lifecycle } from 'recompose';
 import { Form, FormControl, Button } from 'react-bootstrap';
+import ReactMarkdown from 'react-markdown';
 
 import busyLoading from '../busyLoading';
 
 const Comments = ({getThoComments, postThoComment, thoIndex, comments, ...props}) => {
-  const { onChange, onSubmit, onDelete, commentState } = props;
+  const { onChange, onSubmit, onDelete, commentState, toggleCommentShow, commentShow } = props;
   const commentsList = comments.map((comment, index) => {
+    const citeMarkdown = `  \n*&nbsp;&nbsp;${comment.postedUser.profile.email} gửi lúc:
+    ${new Date(comment.postedAt || null).toLocaleString()}*`;
     return (
-      <div key={`comment_${index}`}>
-        <p style={{borderLeft: "solid 1px lightgrey", paddingLeft: "3px"}}>{comment.text}</p>
-        {/* [X] TODO: using user profile*/}
-        <cite style={{paddingLeft: "5px"}}>{`${comment.postedUser.profile.email}
-          gửi lúc: ${new Date(comment.postedAt || null).toLocaleString()} | `}</cite>
-        <Button bsStyle="link" onClick={() => onDelete(comment._id)}
+      <div key={`comment_${index}`} style={{borderLeft: "solid 1px lightgrey", paddingLeft: "3px"}}>
+        <ReactMarkdown source={comment.text + citeMarkdown}  />
+        <Button className="pull-right" bsStyle="link" onClick={() => onDelete(comment._id)}
           disabled={(!props.user.profile) || (props.user.profile.email !== comment.postedUser.profile.email)}>Xóa</Button>
       </div>
     )
   })
   return (
     <div>
-      <h3>Bình luận</h3>
-      {commentsList}
+      <Button bsStyle="link" bsSize="large" onClick={toggleCommentShow}>
+        {commentShow ? `Ẩn bình luận` : `Hiện bình luận`}</Button>
+      { commentShow && commentsList }
+      {commentShow &&
       <Form onSubmit={onSubmit}>
-        <FormControl componentClass="textarea"
+        <FormControl componentClass="textarea" placeholder={`Có thể dùng markdown`}
           value={commentState} onChange={onChange}/>
-        <Button className="pull-right" type="submit" disabled={!props.user.userEmail}
-          bsStyle="link">Gửi bình luận</Button>
+        <Button className="pull-right" type="submit" bsStyle="link"
+          disabled={!props.user.userEmail || !commentState}>Gửi bình luận</Button>
         <div className="clearfix"></div>
       </Form>
+      }
     </div>
   );
 };
@@ -40,6 +43,7 @@ const EnhancedComments = compose(
     }
   }),
   withState('commentState', 'commentHandle', ''),
+  withState('commentShow', 'commentShowHandle', false),
   withHandlers({
     onChange: props => (e) => props.commentHandle(e.target.value),
     onSubmit: props => (e) => {
@@ -55,7 +59,9 @@ const EnhancedComments = compose(
     onDelete: props => (commentId) => {
       window.confirm("Có chắc chắn xóa?") &&
         props.postThoComment(props.thoIndex, {_id: commentId}, 'delete')
-    }
+    },
+    toggleCommentShow: props => () => props.commentShowHandle(currentStatus => !currentStatus)
+
   }),
   branch(
     props => props.busy,
